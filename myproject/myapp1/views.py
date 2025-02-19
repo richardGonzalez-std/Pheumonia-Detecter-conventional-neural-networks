386cee98e870ad9b1633fc90f737b674210af9b3
richardGonzalez-std <zambranoa998@gmail.com>
2025-02-19 19:25:08 -0400

    Cambios en el front y leer prediccion

--- myproject/myapp1/views.py
+++ myproject/myapp1/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from tensorflow import keras
 from keras.models import load_model
 from keras.preprocessing import image
+import io
 import numpy as np
 # Create your views here.
 
-model = load_model('D:/Code_/myproject/myapp1/modelo_1.keras')
+model = load_model('myapp1/modelo_1.keras')
 
 def index(request):
     # model = load_model('myproject/myapp1/modelo_1.keras')
@@ -13,18 +14,17 @@ def index(request):
     
 
 
-def modelo(request, model=model):
-    if request.method == 'POST':
-        img = request.FILES['imagen']
-        img = image.load_img(img, target_size=(200, 200))
+def modelo(request):
+    if 'reset' in request.POST:
+        return render(request, 'myapp1/index.html')
+    if request.method == 'POST' and 'image' in request.FILES:
+        img_file = request.FILES['image']
+        img = image.load_img(io.BytesIO(img_file.read()) , target_size=(200, 200))
         img = image.img_to_array(img)
-        img = np.expand_dims(img, axis=0)
-        img = img/255
+        img = np.expand_dims(img, axis=0) / 255.0
         prediction = model.predict(img)
         prediction = np.argmax(prediction)
-        if prediction == 0:
-            prediction = 'Neumonia'
-        else:
-            prediction = 'Normal'
-        return render(request, 'myapp1/index.html', {'prediction': prediction})
+        labels = ['Neumonia', 'Normal']
+        prediction_label = labels[prediction]
+        return render(request, 'myapp1/index.html', {'prediction': prediction_label})
     return render(request, 'myapp1/index.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp1.urls')),
+    path('', include('myapp1.urls')),
 ]
